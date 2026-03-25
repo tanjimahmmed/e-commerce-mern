88ded7ff1104327a0d12d29c60f4aa4861ecde8e
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2026-03-25 15:28:21 +0600

    add CartDrawer component

--- frontend/src/components/Common/Navbar.jsx
+++ frontend/src/components/Common/Navbar.jsx
@@ -1,49 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router';
 import {HiOutlineUser, HiOutlineShoppingBag, HiBars3BottomRight} from 'react-icons/hi2'
 import SearchBar from './SearchBar';
+import CartDrawer from '../Layout/CartDrawer';
 
 const Navbar = () => {
-  return (
-    <>
-        <nav className='container mx-auto flex items-center justify-between py-4 px-6'>
-            <div className=''>
-                <Link to="/" className='text-2xl font-medium'>
-                    <img src="./logo.svg" alt="" className='w-60 h-20 -ml-2.5' />
-                </Link>
-            </div>
-            <div className="hidden md:flex space-x-6">
-                <Link to="#" className='text-gray-700 hover:text-black text-sm font-medium uppercase'>
-                    Men
-                </Link>
-                <Link to="#" className='text-gray-700 hover:text-black text-sm font-medium uppercase'>
-                    Women
-                </Link>
-                <Link to="#" className='text-gray-700 hover:text-black text-sm font-medium uppercase'>
-                    Top Wear
-                </Link>
-                <Link to="#" className='text-gray-700 hover:text-black text-sm font-medium uppercase'>
-                    Bottom Wear
-                </Link>
-            </div>
-            <div className='flex items-center space-x-4'>
-                <Link to="/profile" className='hover:text-black'>
-                    <HiOutlineUser className='h-6 w-6 text-gray-700'/>
-                </Link>
-                <button className='relative hover:text-black'>
-                    <HiOutlineShoppingBag className='h-6 w-6 text-gray-700'/>
-                    <span className='absolute -top-1 bg-ecom-red text-white text-xs rounded-full px-2 py-0.5'>4</span>
-                </button>
-                <div className="overflow-hidden">
-                    <SearchBar/>
-                </div>
-                <button className='md:hidden'>
-                    <HiBars3BottomRight className='h-6 w-6 text-gray-700'/>
-                </button>
-            </div>
-        </nav>
-    </>
-  )
-}
+    const [drawerOpen, setDrawerOpen] = useState(false);
+
+    const toggleCartDrawer = () => {
+        setDrawerOpen(!drawerOpen);
+    }
+    return (
+            <>
+                <nav className='container mx-auto flex items-center justify-between py-4 px-6'>
+                    <div className=''>
+                        <Link to="/" className='text-2xl font-medium'>
+                            <img src="./logo.svg" alt="" className='w-60 h-20 -ml-2.5' />
+                        </Link>
+                    </div>
+                    <div className="hidden md:flex space-x-6">
+                        <Link to="#" className='text-gray-700 hover:text-black text-sm font-medium uppercase'>
+                            Men
+                        </Link>
+                        <Link to="#" className='text-gray-700 hover:text-black text-sm font-medium uppercase'>
+                            Women
+                        </Link>
+                        <Link to="#" className='text-gray-700 hover:text-black text-sm font-medium uppercase'>
+                            Top Wear
+                        </Link>
+                        <Link to="#" className='text-gray-700 hover:text-black text-sm font-medium uppercase'>
+                            Bottom Wear
+                        </Link>
+                    </div>
+                    <div className='flex items-center space-x-4'>
+                        <Link to="/profile" className='hover:text-black'>
+                            <HiOutlineUser className='h-6 w-6 text-gray-700'/>
+                        </Link>
+                        <button onClick={toggleCartDrawer} className='relative hover:text-black'>
+                            <HiOutlineShoppingBag className='h-6 w-6 text-gray-700'/>
+                            <span className='absolute -top-1 bg-ecom-red text-white text-xs rounded-full px-2 py-0.5'>4</span>
+                        </button>
+                        <div className="overflow-hidden">
+                            <SearchBar/>
+                        </div>
+                        <button className='md:hidden'>
+                            <HiBars3BottomRight className='h-6 w-6 text-gray-700'/>
+                        </button>
+                    </div>
+                </nav>
+                <CartDrawer drawerOpen={drawerOpen} toggleCartDrawer={toggleCartDrawer}/>
+            </>
+        )
+    }
 
 export default Navbar
