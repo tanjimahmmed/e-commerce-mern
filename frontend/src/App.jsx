07e0b4f2c1b784b8e0ff3a6cbde8c89c47061c55
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2026-03-25 11:18:14 +0600

    add react router dom to make route and add Layout - UserLayout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,13 @@
-import {BrowserRouter} from 'react-router'
+import {BrowserRouter, Route, Routes} from 'react-router'
+import UserLayout from './components/Layout/UserLayout'
 
 const App = () => {
   return (
     <BrowserRouter>
-      
+        <Routes>
+            <Route path="/" element={<UserLayout/>}>{/* User */}</Route>
+            <Route>{/* Admin */}</Route>
+        </Routes>
     </BrowserRouter>
   )
 }
