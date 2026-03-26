27d0f84c2b60e82fc8e18048a154e9e369310364
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2026-03-26 12:14:56 +0600

    add CartDrawer Functionality

--- frontend/src/components/Layout/CartDrawer.jsx
+++ frontend/src/components/Layout/CartDrawer.jsx
@@ -1,11 +1,12 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { IoMdClose } from 'react-icons/io';
+import CartContents from '../Cart/CartContents';
 
 const CartDrawer = ({drawerOpen, toggleCartDrawer}) => {
     
 
     return (
-        <div className={`fixed top-0 right-0 w-3/4 sm:w-1/2 md:w-1/4 h-full bg-white shadow-lg transform transition-transform duration-300 flex flex-col z-50 ${drawerOpen ? "translate-x-0" : "translate-x-full"}`}>
+        <div className={`fixed top-0 right-0 w-3/4 sm:w-1/2 md:w-120 h-full bg-white shadow-lg transform transition-transform duration-300 flex flex-col z-50 ${drawerOpen ? "translate-x-0" : "translate-x-full"}`}>
             <div className="flex justify-end p-4">
                 <button onClick={toggleCartDrawer}>
                     <IoMdClose className='h-6 w-6 text-gray-600'/>
@@ -13,6 +14,7 @@ const CartDrawer = ({drawerOpen, toggleCartDrawer}) => {
             </div>
             <div className='flex-grow p-4 overflow-y-auto'>
                 <h2 className='text-xl font-semibold mb-4'>Your Cart</h2>
+                <CartContents/>
             </div>
             <div className="p-4 bg-white sticky bottom-0">
                 <button className='w-full bg-black text-white py-3 rounded-lg font-semibold hover:bg-gray-800 transition'>Checkout</button>
