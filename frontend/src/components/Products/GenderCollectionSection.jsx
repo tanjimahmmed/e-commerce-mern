e5e0a74af4ef6b7e8653819d88e470954cc6dd22
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2026-03-27 17:37:41 +0600

    Add Highlight section

---
+++ frontend/src/components/Products/GenderCollectionSection.jsx
@@ -0,0 +1,28 @@
+import { Link } from 'react-router'
+import menCollectionImage from '../../assets/mens-collection.webp'
+import womenCollectionImage from '../../assets/womens-collection.webp'
+
+const GenderCollectionSection = () => {
+  return (
+    <section className='py-16 px-4 lg:px-0'>
+        <div className='container mx-auto flex flex-col md:flex-row gap-8'>
+            <div className='relative flex-1'>
+                <img src={womenCollectionImage} alt="Women's collection" className='w-full h-[700px] object-cover' />
+                <div className='absolute bottom-8 left-8 bg-white/90 p-4'>
+                    <h2 className='text-2xl font-bold text-gray-900 mb-3'>Women's Collection</h2>
+                    <Link to="/collections/all?gender=Women" className="text-gray-900 underline">Shop now</Link>
+                </div>
+            </div>
+            <div className='relative flex-1'>
+                <img src={menCollectionImage} alt="Men's collection" className='w-full h-[700px] object-cover' />
+                <div className='absolute bottom-8 left-8 bg-white/90 p-4'>
+                    <h2 className='text-2xl font-bold text-gray-900 mb-3'>Men's Collection</h2>
+                    <Link to="/collections/all?gender=Men" className="text-gray-900 underline">Shop now</Link>
+                </div>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default GenderCollectionSection
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from '../components/Layout/Hero'
+import GenderCollectionSection from '../components/Products/GenderCollectionSection'
 
 const Home = () => {
   return (
     <div>
         <Hero/>
+        <GenderCollectionSection/>
     </div>
   )
 }
